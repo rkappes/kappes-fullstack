604b3462fac356d2a6dfc55727e9a09c2ed4bf3a
rkappes <rkappes18@gmail.com>
2025-01-29 22:38:10 -0800

    fix logic for >100, 0 and <0.

--- hw2/01-coins.js
+++ hw2/01-coins.js
@@ -35,71 +35,47 @@ const calculateChange = function calculateChange(amount) {
 
   let result = "";
 
-  console.log(`Initial amount is ${amount}`);
+  //console.log(`Initial amount is ${amount}`);
 
   if (amount > 100) {
-    result =
-      "Amount is more than $100, please enter an amount less than or equal to 100";
+    result = `\$${amount} ==> Error: number too large, please enter an amount less than or equal to 100`;
+  } else if (amount < 0) {
+    result = `\$${amount} ==> Error: amount is negative, not valid`;
+  } else if (amount === 0) {
+    result = `\$${amount} ==> ${dollars} dollars, ${quarters} quarters, ${dime} dimes, ${nickel} nickels, ${penny} pennies`;
+  } else {
+    result = `\$${amount} ==> `;
+    dollars = Math.floor(amount);
+    result += format_output("dollar", dollars);
+
+    cents = amount - dollars;
+    cents = parseFloat(cents.toFixed(2));
+    //console.log(`After subtracting dollars, cents is now ${cents}`);
+
+    quarters = Math.floor(cents / 0.25);
+    result += format_output("quarter", quarters);
+
+    cents -= quarters * 0.25;
+    cents = parseFloat(cents.toFixed(2));
+    //console.log(`After subtracting quarters cents is now ${cents}`);
+
+    dime = Math.floor(cents / 0.1);
+    result += format_output("dime", dime);
+
+    cents -= dime * 0.1;
+    cents = parseFloat(cents.toFixed(2));
+    //console.log(`After subtracting dimes, cents is now ${cents}`);
+
+    nickel = Math.floor(cents / 0.05);
+    result += format_output("nickel", nickel);
+
+    cents -= nickel * 0.05;
+    cents = parseFloat(cents.toFixed(2));
+    //console.log(`After subtracting nickels, cents is now ${cents}`);
+
+    penny = Math.floor(cents / 0.01);
+    result += format_output("penny", penny);
   }
-
-  if (amount < 0) {
-    result = "Amount is negative, not valid";
-  }
-
-  if (amount === 0) {
-    result = `${dollars} dollars, ${quarters} quarters, ${dime} dimes, ${nickel} nickels, ${penny} pennies are needed`;
-  }
-
-  result = `\$${amount} ==> `;
-  dollars = Math.floor(amount);
-  result += format_output("dollar", dollars);
-
-  //   if (dollars > 1 || dolars === 0) {
-  //     result = `${dollars} dollars`;
-  //   } else {
-  //     result = `${dollars} dollar`;
-  //   }
-
-  cents = amount - dollars;
-  cents = parseFloat(cents.toFixed(2));
-  console.log(`After subtracting dollars, cents is now ${cents}`);
-
-  quarters = Math.floor(cents / 0.25);
-  result += format_output("quarter", quarters);
-
-  //   if (quarters > 1 || quarters === 0) {
-  //     result += `${quarters} quarters`;
-  //   } else {
-  //     result += `${quarters} quarter`;
-  //   }
-
-  cents -= quarters * 0.25;
-  cents = parseFloat(cents.toFixed(2));
-  console.log(`After subtracting quarters cents is now ${cents}`);
-
-  dime = Math.floor(cents / 0.1);
-  result += format_output("dime", dime);
-
-  //   if (dime > 1 || dime === 0) {
-  //     result += `${dime} dimes`;
-  //   } else {
-  //     result += `${dime} dime`;
-  //   }
-
-  cents -= dime * 0.1;
-  cents = parseFloat(cents.toFixed(2));
-  console.log(`After subtracting dimes, cents is now ${cents}`);
-
-  nickel = Math.floor(cents / 0.05);
-  result += format_output("nickel", nickel);
-
-  cents -= nickel * 0.05;
-  cents = parseFloat(cents.toFixed(2));
-  console.log(`After subtracting nickels, cents is now ${cents}`);
-
-  penny = Math.floor(cents / 0.01);
-  result += format_output("penny", penny);
-
   return result;
 };
 
@@ -112,3 +88,8 @@ console.log(calculateChange(150.11));
 // $150.11 ==> Error: the number is too large
 
 // Add additional test cases here
+console.log(calculateChange(0.0));
+// $0.00 ==> 0 dollars, 0 quarters, 0 dimes, 0 nickels, 0 pennies
+
+console.log(calculateChange(-3));
+// $-3 ==> Error: amount is negative, not valid
