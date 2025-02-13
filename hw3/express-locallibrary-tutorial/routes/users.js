c4018dbf34b557ad81e30ed4d33d9f16b0fb6046
rkappes <rkappes18@gmail.com>
2025-02-12 22:49:53 -0800

    add challenge

--- hw3/express-locallibrary-tutorial/routes/users.js
+++ hw3/express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,8 @@ router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.get("/cool/", function (req, res, next) {
+  res.set({ "Content-Type": "text/html" });
+  res.send("<h1>You're so cool</h1>");
+});
 module.exports = router;
